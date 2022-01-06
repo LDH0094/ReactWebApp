d5965a0a88b0f2c5091c00eb1a268683ed4c0304
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-01-06 17:48:00 +0900

    bootstrap 사용~

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,13 @@
 import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 //백엔드에서 날라온 데이터가 성공적으로 프론트에 왔습니다!
 function App () {
-  const [message, setMessage] = useState("");
-
-  // useEffect(() => {
-  //   fetch('/api/hello')
-  //       .then(response => response.text())
-  //       .then(message => {
-  //         setMessage(message);
-  //       });
-  // },[])
   return (
-      <div className="App">
-          <form action="/action_page.php">
-              <div className="form-group">
-                  <label htmlFor="email">Email address:</label>
-                  <input type="email" className="form-control" placeholder="Enter email" id="email">
-              </div>
-              <div className="form-group">
-                  <label htmlFor="pwd">Password:</label>
-                  <input type="password" className="form-control" placeholder="Enter password" id="pwd">
-              </div>
-              <div className="form-group form-check">
-                  <label className="form-check-label">
-                      <input className="form-check-input" type="checkbox"> Remember me
-                  </label>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-      </div>
   )
 }
 
