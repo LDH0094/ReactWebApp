8c4afc539aa1cee86680dd3b184b0babd0622eb9
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-01-07 15:00:05 +0900

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { AccountBox } from './components/accountBox';
+
 //백엔드에서 날라온 데이터가 성공적으로 프론트에 왔습니다!
 function App () {
   return (
     <LogIn></LogIn>
-    <AccountBox>sdasd</AccountBox>
+
   )
 }
 
--- frontend/src/components/accountBox/index.jsx
+++ frontend/src/components/accountBox/index.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
+//아마도 패딩?
+const BoxContainer = styled.div`
+    width: 280px;
+    min-height: 555px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 19px;
+    background-color: #fff;
+    box-shawdow: 0 0 2px rgba(15, 15, 15, 0.28);
+    position: relative;
+    overflow: hidden;
+`;
 
+const TopContainer = styled.div`
+    width: 100px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 0 1.8em;
+    padding-bottom: 5em;
+`;
 
+const BackDrop = styled.div`
+    width: 160%;
+    height: 550px;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    border-radius: 50%;
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(37,121,9,1) 0%, rgba(24,127,43,1) 0%, rgba(0,212,255,1) 98%);
+    
+    `;
 
 export function AccountBox(props){
-    return (
-        <h1>dasd</h1>
-    )
+    return <BoxContainer>
+        <TopContainer>
+            <BackDrop />
+        </TopContainer>
+    </BoxContainer>
 }
