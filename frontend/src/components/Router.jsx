97bc468b415bdfd630769c5059020dba77857e9c
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-01-11 12:24:57 +0900

    css 추가및 index 변경

--- frontend/src/components/Router.jsx
+++ frontend/src/components/Router.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
-import  Hello  from "./rollingPaper/blankPage";
+import  {Hello}  from "./rollingPaper/blankPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<App/>} />
         <Route path="/rolling" element={<Hello />} />
       </Routes>
     </BrowserRouter>
   );
 };
-
 export default Router;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import Router from './components/Router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router/>
   </React.StrictMode>,
   document.getElementById('root')
 );
