a8a864075527b0655d2f027adfc0f9ce8549752a
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-01-12 11:11:50 +0900

    간단한 깃

--- frontend/src/components/Router.jsx
+++ frontend/src/components/Router.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
 import  {Paper}  from "./rollingPaper/blankPage";
+import { Widget } from "./widget/widget";
+
 
 const Router = () => {
   return (
@@ -9,7 +11,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<App/>} />
         <Route path="/rolling" element={<Paper/>} />
-      </Routes>
+        <Route path="/rolling/1" element={<Widget/>}/>
+      </Routes> 
     </BrowserRouter>
   );
 };
---
+++ frontend/src/components/widget/widget.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import "./widget.css";
+
+
+
+
+
+export function Widget(){
+    
+    return(
+
+    );
+}
