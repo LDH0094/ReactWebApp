cf3363c231c7603efa29664327a674e721ef25cc
amyhyolee <amyhyolee@berkeley.edu>
2022-01-13 10:40:31 +0900

    note 추가

--- frontend/src/components/Router.jsx
+++ frontend/src/components/Router.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
+import Apple from "../apple"
 import { Paper } from "./rollingPaper/blankPaper";
 
 
@@ -9,7 +10,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App/>} />
-        <Route path="/rolling" element={<Paper/>} />
+        <Route path="/rolling" element={<Apple/>} />
       </Routes>
     </BrowserRouter>
   );
